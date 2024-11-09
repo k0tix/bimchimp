2c927fa648d2fdd1f0c8adc28cf770a45cf7fda9
Antti Nousiainen <anttinousiai@gmail.com>
2024-11-09 21:32:15 +0200

    removed docker for backend, added code related to file conversion

--- backend/php/public/index.php
+++ backend/php/public/index.php
@@ -4,7 +4,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-$db = new PDO('sqlite:/db/db.sqlite');
+$db = new PDO('sqlite:../../db/db.sqlite');
 
 // Instantiate App
 $app = AppFactory::create();
@@ -14,13 +14,46 @@ $app->addErrorMiddleware(true, true, true);
 
 // Add routes
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    $response->getBody()->write(<<<HTML
+        <form action="/files/add" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" accept=".ifc">
+            <button type="submit">Send</button>
+        </form>
+    HTML);
+
     return $response;
 });
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+$app->post('/files/add', function (Request $request, Response $response, $args) {
+    // datapoikien koodi TODO
+
+
+
+
+    // muunto ifc-> weetabix
+    $newFile = tempnam(__DIR__ . '/../tmp', 'ifc');
+    $oldFile = $newFile . ".ifc";
+    /** @var UploadedFile $file */
+    $file = $request->getUploadedFiles()["file"];
+    $file->moveTo($oldFile);
+
+    $out = "";
+    $cmd = __DIR__ . "/../WexBIM/win-x64/CreateWexBIM.exe $oldFile $newFile";
+    exec($cmd, $out);
+
+
+    // tietokantaan data TODO
+
+    return $response;
+});
+
+$app->get('/files/list', function (Request $request, Response $response) {
+    return $response;
+});
+
+$app->get('/files/{id}', function (Request $request, Response $response, $args) {
+    $id = $args['id'];
+
     return $response;
 });
 
